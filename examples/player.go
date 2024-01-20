6ca8e0c5a19b4ca2bde2509adc920906dfe72105
Anton Schnitzer <32676027+antonschnitzer@users.noreply.github.com>
2024-01-20 15:53:56 +0100

    player examples

---
+++ examples/player.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+
+	"github.com/aaantiii/clash"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	creds := make(clash.Credentials)
+	emailStr := os.Getenv("EMAILS")
+	passwordStr := os.Getenv("PASSWORDS")
+	emails := strings.Split(emailStr, ",")
+	passwords := strings.Split(passwordStr, ",")
+
+	for i, email := range emails {
+		creds[email] = passwords[i]
+	}
+
+	client, err := clash.NewClient(creds)
+	if err != nil {
+		panic(err)
+	}
+
+	// get a player by tag
+	player, err := client.GetPlayer("#8QYG8CJ0")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(player.Name)
+
+	// concurrently get multiple players by tag
+	players, err := client.GetPlayers("#8QYG8CJ0", "#Q9RY8YRYJ", "#G0CUP9UPU", "#QVLQLQUC2")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(players.Tags())
+
+	// verify player with api token
+	verification, err := client.VerifyPlayer("#8QYG8CJ0", "apiToken")
+	if err != nil {
+		panic(err)
+	}
+	if verification.IsOk() {
+		fmt.Println("verification successful")
+	}
+}
