4a9916706304228e3abc560d763509dd1458eebc
Anton Schnitzer <32676027+antonschnitzer@users.noreply.github.com>
2024-01-20 15:54:45 +0100

    Create images.go

---
+++ images.go
@@ -0,0 +1,8 @@
+package clash
+
+type ImageURLs struct {
+	Tiny   string `json:"tiny,omitempty"`
+	Small  string `json:"small,omitempty"`
+	Medium string `json:"medium,omitempty"`
+	Large  string `json:"large,omitempty"`
+}
