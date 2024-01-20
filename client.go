67384a697c5051253ce6a918c9a402f6b89accb8
Anton Schnitzer <32676027+antonschnitzer@users.noreply.github.com>
2024-01-20 15:55:56 +0100

    restructured

--- client.go
+++ client.go
@@ -16,7 +16,7 @@ type Client struct {
 	accounts []*APIAccount
 	rc       *resty.Client
 	ipAddr   string
-	keyIndex APITokenIndexer
+	keyIndex APIKeyIndex
 	mu       sync.Mutex
 }
 
@@ -40,58 +40,59 @@ func newClient(creds Credentials) (*Client, error) {
 	if err := client.updateIPAddr(); err != nil {
 		return nil, err
 	}
-
 	if err := client.updateAccounts(); err != nil {
 		return nil, err
 	}
-
 	return client, nil
 }
 
-func (c *Client) do(method string, route Endpoint, body any, retry bool) ([]byte, error) {
-	req := c.withAuth(c.newDefaultRequest())
+func (h *Client) do(method, url string, body any, retry bool) ([]byte, error) {
+	if useCache {
+		if data, ok := readCache(url); ok {
+			return data, nil
+		}
+	}
+
+	req := h.withAuth(h.newDefaultRequest())
 	if method == http.MethodPost && body != nil {
 		req.SetBody(body)
 	}
 
-	res, err := req.Execute(method, route.URL())
+	res, err := req.Execute(method, url)
 	if err != nil {
 		return nil, err
 	}
 
-	clientErr := &ClientError{Status: res.StatusCode()}
-	if clientErr.Status >= 300 {
-		if err = sonic.Unmarshal(res.Body(), &clientErr.APIError); err != nil {
-			return nil, err
-		}
+	if res.StatusCode() < 300 && res.StatusCode() >= 200 {
+		cacheResponse(url, res)
+		return res.Body(), nil
 	}
 
+	clientErr := &ClientError{Status: res.StatusCode(), APIError: &APIError{}}
+	if err = sonic.Unmarshal(res.Body(), &clientErr.APIError); err != nil {
+		return nil, err
+	}
 	if res.StatusCode() == http.StatusForbidden {
 		if !retry {
 			return nil, clientErr
 		}
 
 		if clientErr.APIError.Reason == ReasonInvalidIP {
-			if err = c.updateIPAddr(); err != nil {
-				return nil, err
+			if err = h.updateIPAddr(); err != nil {
+				return nil, newClientErr(err)
 			}
-			if err = c.updateAccounts(); err != nil {
-				return nil, err
+			if err = h.updateAccounts(); err != nil {
+				return nil, newClientErr(err)
 			}
-			return c.do(method, route, body, false)
+			return h.do(method, url, body, false)
 		}
 	}
 
-	return res.Body(), nil
+	return nil, clientErr
 }
 
-func (c *Client) withAuth(req *resty.Request) *resty.Request {
-	key := c.getKey()
-	return req.SetHeader("Authorization", fmt.Sprintf("Bearer %s", key))
-}
-
-func (c *Client) updateIPAddr() error {
-	res, err := c.rc.R().Get(IPifyEndpoint)
+func (h *Client) updateIPAddr() error {
+	res, err := h.rc.R().Get(IPifyEndpoint)
 	if err != nil {
 		return err
 	}
@@ -100,15 +101,18 @@ func (c *Client) updateIPAddr() error {
 	if res.StatusCode() != http.StatusOK {
 		return errors.New(body)
 	}
+	if body == h.ipAddr {
+		return nil
+	}
 
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.ipAddr = body
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	h.ipAddr = body
 	return nil
 }
 
-func (c *Client) login(account *APIAccount) error {
-	res, err := c.newDefaultRequest().SetBody(account.Credentials).Post(DevLoginEndpoint.URL())
+func (h *Client) login(account *APIAccount) error {
+	res, err := h.newDefaultRequest().SetBody(account.Credentials).Post(DevLoginEndpoint.URL())
 	if err != nil {
 		return err
 	}
@@ -120,12 +124,12 @@ func (c *Client) login(account *APIAccount) error {
 	return sonic.Unmarshal(res.Body(), &account)
 }
 
-func (c *Client) updateAccounts() error {
-	for _, account := range c.accounts {
-		if err := c.login(account); err != nil {
+func (h *Client) updateAccounts() error {
+	for _, account := range h.accounts {
+		if err := h.login(account); err != nil {
 			return err
 		}
-		if err := c.updateAccountKeys(account); err != nil {
+		if err := h.updateAccountKeys(account); err != nil {
 			return err
 		}
 	}
@@ -134,8 +138,8 @@ func (c *Client) updateAccounts() error {
 }
 
 // getAccountKeys retrieves the API keys for the given account and sets APIAccount.Keys.
-func (c *Client) getAccountKeys(account *APIAccount) error {
-	res, err := c.newDefaultRequest().Post(DevKeyListEndpoint.URL())
+func (h *Client) getAccountKeys(account *APIAccount) error {
+	res, err := h.newDefaultRequest().Post(DevKeyListEndpoint.URL())
 	if err != nil {
 		return err
 	}
@@ -149,18 +153,65 @@ func (c *Client) getAccountKeys(account *APIAccount) error {
 		return err
 	}
 
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	account.Keys = body.Keys
 	return nil
 }
 
-func (c *Client) createAccountKey(account *APIAccount, index int) error {
+func (h *Client) updateAccountKeys(account *APIAccount) error {
+	if err := h.getAccountKeys(account); err != nil {
+		return err
+	}
+
+	errChan := make(chan error, keysPerAccount)
+	var freeKeyIndexes []int
+	var wg sync.WaitGroup
+	for i, key := range account.Keys {
+		if key == nil {
+			freeKeyIndexes = append(freeKeyIndexes, i)
+			continue
+		}
+		if !slices.Contains(key.CidrRanges, h.ipAddr) {
+			wg.Add(1)
+			go func(key *APIKey, i int) {
+				defer wg.Done()
+				if err := h.revokeAccountKey(key); err != nil {
+					errChan <- err
+					return
+				}
+				if err := h.createAccountKey(account, i); err != nil {
+					errChan <- err
+					return
+				}
+			}(key, i)
+		}
+	}
+	wg.Wait()
+
+	for i := range freeKeyIndexes {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			if err := h.createAccountKey(account, keysPerAccount-i); err != nil {
+				errChan <- err
+			}
+		}(i)
+	}
+	wg.Wait()
+
+	if len(errChan) > 0 {
+		return <-errChan
+	}
+	return nil
+}
+
+func (h *Client) createAccountKey(account *APIAccount, index int) error {
 	desc := fmt.Sprintf("Created at %s by clash.go", time.Now().UTC().Round(time.Minute).String())
-	res, err := c.newDefaultRequest().SetBody(&APIKey{
+	res, err := h.newDefaultRequest().SetBody(&APIKey{
 		Name:        "clash.go",
 		Description: desc,
-		CidrRanges:  []string{c.ipAddr},
+		CidrRanges:  []string{h.ipAddr},
 		Scopes:      []string{"clash"},
 	}).Post(DevKeyCreateEndpoint.URL())
 	if err != nil {
@@ -176,65 +227,17 @@ func (c *Client) createAccountKey(account *APIAccount, index int) error {
 		return err
 	}
 
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	account.Keys[index] = keyRes.Key
 	return nil
 }
 
-func (c *Client) updateAccountKeys(account *APIAccount) error {
-	if err := c.getAccountKeys(account); err != nil {
-		return err
-	}
-
-	errChan := make(chan error, keysPerAccount)
-	var freeKeysIndex []int
-	var wg sync.WaitGroup
-	for i, key := range account.Keys {
-		if key == nil {
-			freeKeysIndex = append(freeKeysIndex, i)
-			continue
-		}
-		if !slices.Contains(key.CidrRanges, c.ipAddr) {
-			wg.Add(1)
-			go func(key *APIKey, i int) {
-				defer wg.Done()
-				if err := c.revokeAccountKey(key); err != nil {
-					errChan <- err
-					return
-				}
-				if err := c.createAccountKey(account, i); err != nil {
-					errChan <- err
-					return
-				}
-			}(key, i)
-		}
-	}
-	wg.Wait()
-
-	for i := range freeKeysIndex {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			err := c.createAccountKey(account, keysPerAccount-i)
-			if err != nil {
-				errChan <- err
-			}
-		}(i)
-	}
-	wg.Wait()
-
-	if len(errChan) > 0 {
-		return <-errChan
-	}
-	return nil
-}
-
-func (c *Client) revokeAccountKey(key *APIKey) error {
+func (h *Client) revokeAccountKey(key *APIKey) error {
 	payload := struct {
 		ID string `json:"id"`
 	}{ID: key.ID}
-	res, err := c.newDefaultRequest().SetBody(payload).Post(DevKeyRevokeEndpoint.URL())
+	res, err := h.newDefaultRequest().SetBody(payload).Post(DevKeyRevokeEndpoint.URL())
 	if err != nil {
 		return err
 	}
@@ -246,22 +249,26 @@ func (c *Client) revokeAccountKey(key *APIKey) error {
 	return nil
 }
 
-func (c *Client) getKey() string {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+func (h *Client) getKey() string {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
-	key := c.accounts[c.keyIndex.AccountIndex].Keys[c.keyIndex.TokenIndex]
-	c.keyIndex.TokenIndex = (c.keyIndex.TokenIndex + 1) % len(c.accounts[c.keyIndex.AccountIndex].Keys)
-	if c.keyIndex.TokenIndex == len(c.accounts[c.keyIndex.AccountIndex].Keys)-1 {
-		c.keyIndex.AccountIndex = (c.keyIndex.AccountIndex + 1) % len(c.accounts)
+	key := h.accounts[h.keyIndex.AccountIndex].Keys[h.keyIndex.KeyIndex]
+	if h.keyIndex.KeyIndex == len(h.accounts[h.keyIndex.AccountIndex].Keys)-1 {
+		h.keyIndex.AccountIndex = (h.keyIndex.AccountIndex + 1) % len(h.accounts)
 	}
-
+	h.keyIndex.KeyIndex = (h.keyIndex.KeyIndex + 1) % len(h.accounts[h.keyIndex.AccountIndex].Keys)
 	return key.Key
 }
 
-func (c *Client) newDefaultRequest() *resty.Request {
-	return c.rc.R().
+func (h *Client) newDefaultRequest() *resty.Request {
+	return h.rc.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("Content-Type", "application/json").
 		SetHeader("User-Agent", "clash.go")
+}
+
+func (h *Client) withAuth(req *resty.Request) *resty.Request {
+	key := h.getKey()
+	return req.SetHeader("Authorization", fmt.Sprintf("Bearer %s", key))
 }
